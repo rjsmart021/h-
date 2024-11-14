984c9619d59eb21657a73246b3aa429e8378b90a
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-13 23:40:53 -0600

    Create marvelAPI.js

---
+++ marvelAPI.js
@@ -0,0 +1,3 @@
+//1. Exploring Asynchronous JavaScript
+//Obtain an API key from the Marvel Comics API website. Investigate and understand the necessary configurations required to authenticate requests to the API.
+
